c9bca28ee450251e1796a8ff672eb12a0bfc1351
chuenningyeh <chuenningyeh@gmail.com>
2023-10-23 12:50:33 +0800

    測試呼叫查詢幣別對應表資料 API，並顯示其內容

--- src/main/java/com/example/entity/Currency.java
+++ src/main/java/com/example/entity/Currency.java
@@ -1,14 +1,13 @@
 package com.example.entity;
 
 
-
-
-
 import javax.persistence.*;
+import java.math.BigDecimal;
 
 
 @Entity
 public class Currency {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -18,9 +17,15 @@ public class Currency {
 
     private String name;
 
-    public Currency(String code, String name) {
+    private BigDecimal price;
+
+    public Currency() {
+    }
+
+    public Currency(String code, String name, BigDecimal price) {
         this.code = code;
         this.name = name;
+        this.price = price;
     }
 
     public Long getId() {
@@ -46,5 +51,34 @@ public class Currency {
     public void setName(String name) {
         this.name = name;
     }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Currency currency = (Currency) o;
+
+        return code.equals(currency.code);
+    }
+
+    @Override
+    public int hashCode() {
+        return code.hashCode();
+    }
+
+
 }
 
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,43 +1,81 @@
 package com.example.demo;
 
 import com.example.controller.CurrencyController;
-import com.example.controller.PriceInfoController;
-import com.example.dto.CurrencyInfo;
+import com.example.dto.Bpi;
+import com.example.dto.PriceInfo;
 import com.example.entity.Currency;
+import com.example.enumeration.Coin;
+import com.example.repository.CurrencyRepository;
 import com.example.service.CurrencyService;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.junit.Before;
+import com.example.service.PriceInfoService;
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.Date;
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
+@SpringBootTest
 @RunWith(SpringRunner.class)
+@ComponentScan(basePackages = "com.example")
 class DemoApplicationTests {
     @Autowired
     private CurrencyService currencyService;
-    @Before
-    public void setUp() {
-        //save Data to H2 db
+    @Autowired
+    private PriceInfoService priceInfoService;
+    @Autowired
+    CurrencyRepository repository;
+    @Autowired
+    private CurrencyController currencyController;
 
+    @BeforeEach
+    public void setUp() {
+        //save CoinDeskAPI Data to H2 db
+        PriceInfo priceInfo = priceInfoService.getCoinDeskAPI();
+        Map<String, Bpi> bpiMap = priceInfo.getBpi();
+        for (String coinKey : bpiMap.keySet()) {
+            Bpi bpi = bpiMap.get(coinKey);
+            Currency currency = new Currency(coinKey, Coin.valueOf(coinKey).getNameCh(), bpi.getRate());
+            repository.save(currency);
+        }
+        //測試資料是否都有存到資料庫
+        List<Currency> savedCurrencies = repository.findAll();
+        Assert.assertFalse(savedCurrencies.isEmpty());
+        Assert.assertTrue(savedCurrencies.stream().anyMatch(currency -> "USD".equals(currency.getCode())));
+        Assert.assertTrue(savedCurrencies.stream().anyMatch(currency -> "GBP".equals(currency.getCode())));
+        Assert.assertTrue(savedCurrencies.stream().anyMatch(currency -> "EUR".equals(currency.getCode())));
     }
+
+    //    測試呼叫查詢幣別對應表資料 API，並顯示其內容
+    @Test
+    public void testGetCurrencyList() {
+//        測試呼叫查詢幣別/list API, 資料是不是等於 coindesk API
+        List<Currency> resultList = currencyController.getAllCurrencies();
+        //呼叫coindesk API
+        List<Currency> currencyList = getCoinDeskAPI();
+//        比對其資料是否一致
+        assertThat(resultList).containsExactlyInAnyOrderElementsOf(currencyList);
+    }
+
+    private List<Currency> getCoinDeskAPI() {
+        List<Currency> currencyList = new ArrayList<>();
+        PriceInfo priceInfo = priceInfoService.getCoinDeskAPI();
+        Map<String, Bpi> bpiMap = priceInfo.getBpi();
+        for (String coinKey : bpiMap.keySet()) {
+            Bpi bpi = bpiMap.get(coinKey);
+            Currency currency = new Currency(coinKey, Coin.valueOf(coinKey).getNameCh(), bpi.getRate());
+            currencyList.add(currency);
+        }
+        return currencyList;
+    }
+
+
 }
